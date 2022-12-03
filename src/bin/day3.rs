5de798fea4e0e7aec3b8ac95e7a9fd9b6c70c28a
kalkyl <henrik.alser@me.com>
2022-12-03 10:27:42 +0100

    Cleanup

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,17 +1,17 @@
 use std::{fs::read_to_string, io::Error};
 
-fn find_item(input: &str) -> char {
-    let (first, second) = input.split_at(input.len() / 2);
+fn find_item(bag: &str) -> char {
+    let (first, second) = bag.split_at(bag.len() / 2);
     first
         .chars()
         .find(|&a| second.chars().any(|b| b == a))
         .unwrap()
 }
 
-fn find_badge(input: &[&str]) -> char {
-    input[0]
+fn find_badge(group: &[&str]) -> char {
+    group[0]
         .chars()
-        .find(|&a| input[1].chars().any(|b| b == a) && input[2].chars().any(|c| c == a))
+        .find(|&a| group[1].chars().any(|b| b == a) && group[2].chars().any(|c| c == a))
         .unwrap()
 }
 
@@ -23,13 +23,13 @@ fn prio(c: char) -> u32 {
 }
 
 fn main() -> Result<(), Error> {
-    let entries = read_to_string("./input/3.txt")?;
-    let entries: Vec<_> = entries.lines().collect();
+    let bags = read_to_string("./input/3.txt")?;
+    let bags: Vec<_> = bags.lines().collect();
 
-    let total_a: u32 = entries.iter().map(|l| prio(find_item(l))).sum();
+    let total_a: u32 = bags.iter().map(|b| prio(find_item(b))).sum();
     println!("A: {:?}", total_a);
 
-    let total_b: u32 = entries.chunks(3).map(|l| prio(find_badge(l))).sum();
+    let total_b: u32 = bags.chunks(3).map(|g| prio(find_badge(g))).sum();
     println!("B: {:?}", total_b);
     Ok(())
 }
