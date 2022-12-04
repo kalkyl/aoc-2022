f1aa4d04605d1a33d97516d81b0f549b0ff630de
kalkyl <henrik.alser@me.com>
2022-12-04 10:34:59 +0100

    Cleanup

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,25 +1,21 @@
-use std::{fs::read_to_string, io::Error};
-type Section = (u32, u32);
+use std::{fs::read_to_string, io::Error, ops::RangeInclusive};
+type Section = RangeInclusive<u32>;
 
 fn pair(line: &str) -> (Section, Section) {
     fn parse(s: &str) -> Section {
         let (start, end) = s.split_once('-').unwrap();
-        (start.parse().unwrap(), end.parse().unwrap())
+        start.parse().unwrap()..=end.parse().unwrap()
     }
     let (a, b) = line.split_once(',').unwrap();
     (parse(a), parse(b))
 }
 
 fn overlaps_full((a, b): &&(Section, Section)) -> bool {
-    (a.0..=a.1).contains(&b.0) && (a.0..=a.1).contains(&b.1)
-        || (b.0..=b.1).contains(&a.0) && (b.0..=b.1).contains(&a.1)
+    a.contains(&b.start()) && a.contains(&b.end()) || b.contains(&a.start()) && b.contains(&a.end())
 }
 
 fn overlaps_any((a, b): &&(Section, Section)) -> bool {
-    (a.0..=a.1).contains(&b.0)
-        || (a.0..=a.1).contains(&b.1)
-        || (b.0..=b.1).contains(&a.0)
-        || (b.0..=b.1).contains(&a.1)
+    a.contains(&b.start()) || a.contains(&b.end()) || b.contains(&a.start()) || b.contains(&a.end())
 }
 
 fn main() -> Result<(), Error> {
