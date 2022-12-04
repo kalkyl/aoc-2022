a50096943331c1237d7af799211d752e89a34aa9
kalkyl <henrik.alser@me.com>
2022-12-04 09:26:15 +0100

    fmt

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,7 +1,8 @@
 use std::{fs::read_to_string, io::Error};
+type Section = (u32, u32);
 
-fn pair(line: &str) -> ((u32, u32), (u32, u32)) {
-    fn parse(s: &str) -> (u32, u32) {
+fn sections(line: &str) -> (Section, Section) {
+    fn parse(s: &str) -> Section {
         let (start, end) = s.split_once('-').unwrap();
         (start.parse().unwrap(), end.parse().unwrap())
     }
@@ -9,18 +10,23 @@ fn pair(line: &str) -> ((u32, u32), (u32, u32)) {
     (parse(a), parse(b))
 }
 
-fn overlaps_all((a, b): &&((u32, u32), (u32, u32))) -> bool {
+fn overlaps_all((a, b): &&(Section, Section)) -> bool {
     (a.0..=a.1).contains(&b.0) && (a.0..=a.1).contains(&b.1)
         || (b.0..=b.1).contains(&a.0) && (b.0..=b.1).contains(&a.1)
 }
 
-fn overlaps_any((a, b): &&((u32, u32), (u32, u32))) -> bool {
-    (a.0..=a.1).contains(&b.0) || (a.0..=a.1).contains(&b.1)
-        || (b.0..=b.1).contains(&a.0) || (b.0..=b.1).contains(&a.1)
+fn overlaps_any((a, b): &&(Section, Section)) -> bool {
+    (a.0..=a.1).contains(&b.0)
+        || (a.0..=a.1).contains(&b.1)
+        || (b.0..=b.1).contains(&a.0)
+        || (b.0..=b.1).contains(&a.1)
 }
 
 fn main() -> Result<(), Error> {
-    let pairs: Vec<_> = read_to_string("./input/4.txt")?.lines().map(pair).collect();
+    let pairs: Vec<_> = read_to_string("./input/4.txt")?
+        .lines()
+        .map(sections)
+        .collect();
 
     let count_a = pairs.iter().filter(overlaps_all).count();
     println!("A: {:?}", count_a);
