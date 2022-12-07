4d53111bcec781d0d83af0b211a57be322a8acc7
kalkyl <henrik.alser@me.com>
2022-12-07 02:08:37 +0100

    Cleanup

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,15 +1,17 @@
 use std::{collections::HashSet, fs::read_to_string, io::Error};
 
-fn is_marker(window: &&[(usize, char)]) -> bool {
-    let unique: HashSet<_> = window.iter().map(|(_, c)| c).collect();
-    unique.len() == window.len()
+fn find_marker(input: &str, len: usize) -> usize {
+    fn is_marker(window: &[char]) -> bool {
+        let unique: HashSet<_> = window.iter().collect();
+        unique.len() == window.len()
+    }
+    let input: Vec<_> = input.chars().collect();
+    input.windows(len).position(is_marker).unwrap() + len
 }
 
 fn main() -> Result<(), Error> {
-    let input: Vec<_> = read_to_string("./input/6.txt")?.char_indices().collect();
-    let packet = input.windows(4).find(is_marker).unwrap().last().unwrap().0 + 1;
-    println!("A: {:?}", packet);
-    let message = input.windows(14).find(is_marker).unwrap().last().unwrap().0 + 1;
-    println!("B: {:?}", message);
+    let input = read_to_string("./input/6.txt")?;
+    println!("A: {}", find_marker(&input, 4));
+    println!("B: {}", find_marker(&input, 14));
     Ok(())
 }
