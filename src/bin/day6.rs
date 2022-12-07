4df3c1f561b602a837025e0ef1eb23f899346528
kalkyl <henrik.alser@me.com>
2022-12-07 02:34:25 +0100

    Cleanup

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,17 +1,17 @@
 use std::{collections::HashSet, fs::read_to_string, io::Error};
 
-fn find_marker(input: &str, len: usize) -> usize {
+fn find_marker(input: &str, len: usize) -> Option<usize> {
     fn is_marker(window: &[char]) -> bool {
         let unique: HashSet<_> = window.iter().collect();
         unique.len() == window.len()
     }
     let input: Vec<_> = input.chars().collect();
-    input.windows(len).position(is_marker).unwrap() + len
+    input.windows(len).position(is_marker).map(|i| i + len)
 }
 
 fn main() -> Result<(), Error> {
     let input = read_to_string("./input/6.txt")?;
-    println!("A: {}", find_marker(&input, 4));
-    println!("B: {}", find_marker(&input, 14));
+    println!("A: {}", find_marker(&input, 4).unwrap());
+    println!("B: {}", find_marker(&input, 14).unwrap());
     Ok(())
 }
